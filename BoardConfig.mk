b0a7db22289960f5285b338d0de2ad5dc89592ad
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2012-04-10 02:29:50 +0200

    Add kernel/modules config for future cm9 compat
    Change-Id: Ic7cfcaba65b6af7631466a1c70b8773f58b34c74

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -154,3 +154,8 @@ ifeq ($(BOARD_DEFY_MODEL),DEFY_PLUS)
 BOARD_USE_CID_ROTATE_34 := true
 endif
 
+# If kernel source is present in repo, here is the location
+TARGET_KERNEL_SOURCE := $(ANDROID_BUILD_TOP)/kernel/moto/mb525
+TARGET_KERNEL_CONFIG := mapphone_mb525
+TARGET_KERNEL_MODULES_EXT := $(ANDROID_BUILD_TOP)/device/motorola/jordan/modules
+
